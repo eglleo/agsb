9776f662e287cc807091acfe7a624b1b70fb06fa
eglleo <hyqaqw@gmail.com>
2025-07-12 01:26:22 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytdlala"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "hyqaqw"         # 用户名
+UUID = "51a66044-d592-411d-9a64-920fcfb9be5b"                     # UUID，留空则自动生成
+PORT = 59748                   # Vmess端口，留空或0则自动生成
+DOMAIN = "nba.ziyou123.eu.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiMTE2Mzc1Y2NmMGE5YWFiZjQ1N2FiNDhlOGQ3MTMzYTMiLCJ0IjoiM2YwMWE4NzMtMjJmNC00MTI3LWE0YmUtOWY1NDE1OGIyNmZmIiwicyI6Ik1HSm1ORGN6WWpZdFlXUmlOaTAwTlRjeUxUazVPVEl0WVRobU5HRTRZbUprWkRNMiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
